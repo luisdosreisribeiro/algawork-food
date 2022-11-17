694838fbd5e50219d47c19a1bfa7ac52a44e2c7c
Luis Ribeiro <luis.ribeiro@unicred.com.br>
2022-11-17 17:44:10 -0300

    Endpoint para listar/buscar Restaurantes.

--- src/main/java/com/algaworks/algafood/api/controller/CozinhaController.java
+++ src/main/java/com/algaworks/algafood/api/controller/CozinhaController.java
@@ -4,9 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,7 +16,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.algaworks.algafood.api.controllerapi.model.CozinhasXmlWrapper;
 import com.algaworks.algafood.domain.exception.EntidadeEmUsoException;
 import com.algaworks.algafood.domain.exception.EntidadeNaoEncontradaException;
 import com.algaworks.algafood.domain.model.Cozinha;
@@ -40,12 +37,7 @@ public class CozinhaController {
 	public List<Cozinha>listar(){
 		return cozinhaRepository.listar();
 	}
-	
-	@GetMapping(produces = MediaType.APPLICATION_XML_VALUE)
-	public CozinhasXmlWrapper listarXml(){
-		return new  CozinhasXmlWrapper(cozinhaRepository.listar());
-	}
-	
+
 	
 	@GetMapping("/{id}")
 	public ResponseEntity<Cozinha> buscar(@PathVariable Long id) {
@@ -71,7 +63,7 @@ public class CozinhaController {
 		if(cozinhaAtual != null) {
 			//cozinhaAtual.setNome( cozinha.getNome());
 			BeanUtils.copyProperties(cozinha, cozinhaAtual, "id");
-			cozinhaRepository.salvar(cozinhaAtual);
+			cadastroCozinhaService.salvar(cozinhaAtual);
 			
 			return ResponseEntity.ok(cozinhaAtual);			
 		}
---
+++ src/main/java/com/algaworks/algafood/domain/service/CadastroRestauranteService.java
@@ -0,0 +1,10 @@
+package com.algaworks.algafood.domain.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class CadastroRestauranteService {
+	
+	
+
+}
