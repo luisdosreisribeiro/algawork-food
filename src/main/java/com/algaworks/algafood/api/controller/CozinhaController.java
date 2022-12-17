29dedfd12ed69287668d563a80cf788068665230
Luis Ribeiro <luis.ribeiro@unicred.com.br>
2022-12-17 13:11:51 -0300

    8.2. Lançando exceções customizadas anotadas com @ResponseStatus

--- src/main/java/com/algaworks/algafood/api/controller/CozinhaController.java
+++ src/main/java/com/algaworks/algafood/api/controller/CozinhaController.java
@@ -70,19 +70,26 @@ public class CozinhaController {
 		}
 		return ResponseEntity.notFound().build();		
 	}
+	
 	@DeleteMapping("/{cozinhaId}")
-	public ResponseEntity<Cozinha> remover(@PathVariable Long cozinhaId){
-		try {
-				cadastroCozinhaService.excluir(cozinhaId);
-				return ResponseEntity.noContent().build();
-				
-			}catch(EntidadeNaoEncontradaException e) {
-				return ResponseEntity.status(HttpStatus.NOT_FOUND).build();			
-				
-			}catch(EntidadeEmUsoException e) {
-				return ResponseEntity.status(HttpStatus.CONFLICT).build();
-			}
-					
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public void remover(@PathVariable Long cozinhaId){	
+				cadastroCozinhaService.excluir(cozinhaId);				 
+							
 	}
+	
+//	@DeleteMapping("/{cozinhaId}")
+//	public ResponseEntity<?> remover(@PathVariable Long cozinhaId){
+//		try {
+//				cadastroCozinhaService.excluir(cozinhaId);
+//				return ResponseEntity.noContent().build();
+//				
+//			}catch(EntidadeNaoEncontradaException e) {
+//				return ResponseEntity.notFound().build();			
+//				
+//			}catch(EntidadeEmUsoException e) {
+//				return ResponseEntity.status(HttpStatus.CONFLICT).body(e.getMessage());
+//			}					
+//	}
 
 }
