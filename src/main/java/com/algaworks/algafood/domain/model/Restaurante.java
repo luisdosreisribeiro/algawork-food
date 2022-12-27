7be07751eb6d8c6ffbc29fe69d1a6c5bc5d57561
Luis Ribeiro <luis.ribeiro@unicred.com.br>
2022-12-27 16:07:17 -0300

    9.6. Validando as associações de uma entidade em cascata.

--- src/main/java/com/algaworks/algafood/domain/model/Restaurante.java
+++ src/main/java/com/algaworks/algafood/domain/model/Restaurante.java
@@ -17,6 +17,7 @@ import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
+import javax.validation.Valid;
 import javax.validation.constraints.DecimalMin;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotEmpty;
@@ -54,10 +55,9 @@ public class Restaurante {
 	@Column(name = "taxa_frete", nullable = false)
 	private BigDecimal taxaFrete;
 	
-	
-	//@JsonIgnore
-	@JsonIgnoreProperties("hibernateLazyInitializer")
-	@ManyToOne //(fetch = FetchType.LAZY)
+	@Valid
+	@NotNull
+	@ManyToOne 
 	@JoinColumn(name = "cozinha_id", nullable = false)
 	private Cozinha cozinha;
 	
