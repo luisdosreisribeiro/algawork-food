01ee028bd647943ad06591896efcb672e6d3694d
Luis Ribeiro <luis.ribeiro@unicred.com.br>
2022-12-30 18:38:09 -0300

    Testando primeiro commit na nova branch.

--- src/test/java/com/algaworks/algafood/AlgaworkfoodApiApplicationTests.java
+++ src/test/java/com/algaworks/algafood/AlgaworkfoodApiApplicationTests.java
@@ -1,13 +1,19 @@
 package com.algaworks.algafood;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.Assert.assertFalse;
 
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
 @SpringBootTest
 class AlgaworkfoodApiApplicationTests {
 
 	@Test
-	void contextLoads() {
+	public void contextLoads() {
+		assertFalse(false);
 	}
 
 }
