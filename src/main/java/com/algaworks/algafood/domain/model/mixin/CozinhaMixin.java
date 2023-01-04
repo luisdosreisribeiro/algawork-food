b67f9e2bbcd8af5f2c2b7bc49ca338ea4faf65c2
Luis Ribeiro <luis.ribeiro@unicred.com.br>
2023-01-04 19:52:39 -0300

    11.4. Desafio: usando @JsonIgnoreProperties e Jackson Mixin.

---
+++ src/main/java/com/algaworks/algafood/domain/model/mixin/CozinhaMixin.java
@@ -0,0 +1,17 @@
+package com.algaworks.algafood.domain.model.mixin;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.OneToMany;
+
+import com.algaworks.algafood.domain.model.Restaurante;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+public class CozinhaMixin {
+	
+	
+	@JsonIgnore	
+	private List<Restaurante> restaurantes = new ArrayList<>();
+
+}
