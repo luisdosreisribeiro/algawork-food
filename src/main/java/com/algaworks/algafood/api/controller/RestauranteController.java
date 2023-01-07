2a6e6ff489e7a266f30af68fadd262e3c3b03ab3
Luis Ribeiro <luis.ribeiro@unicred.com.br>
2023-01-07 19:25:00 -0300

    11.17. Mapeando para uma instância destino (e não um tipo) com ModelMapper.

--- src/main/java/com/algaworks/algafood/api/controller/RestauranteController.java
+++ src/main/java/com/algaworks/algafood/api/controller/RestauranteController.java
@@ -77,10 +77,12 @@ public class RestauranteController {
 	public RestauranteModel atualizar(@PathVariable Long restauranteId, 
 			@RequestBody @Valid RestauranteInput restauranteInput){
 		try {
-			Restaurante restaurante =  restauranteInputDisassembler.toDomainObject(restauranteInput);
+			//Restaurante restaurante =  restauranteInputDisassembler.toDomainObject(restauranteInput);
 			
 			Restaurante restauranteAtual = cadastroRestauranteService.buscarOuFalhar(restauranteId);
-			BeanUtils.copyProperties(restaurante, restauranteAtual, "id", "formasPagamento", "endereco", "dataCadastro", "produtos");
+			restauranteInputDisassembler.copyToDomainObject(restauranteInput, restauranteAtual);
+//			
+//			BeanUtils.copyProperties(restaurante, restauranteAtual, "id", "formasPagamento", "endereco", "dataCadastro", "produtos");			
 
 
 			return restauranteModelAssembler.toModel(cadastroRestauranteService.salvar(restauranteAtual));	
