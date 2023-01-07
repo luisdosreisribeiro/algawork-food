e19083e0c277767e6f8d416f98c399747d4a629f
Luis Ribeiro <luis.ribeiro@unicred.com.br>
2023-01-07 17:49:17 -0300

    11.14. Adicionando e usando o ModelMapper.

--- src/main/java/com/algaworks/algafood/api/assembler/RestauranteModelAssembler.java
+++ src/main/java/com/algaworks/algafood/api/assembler/RestauranteModelAssembler.java
@@ -3,6 +3,8 @@ package com.algaworks.algafood.api.assembler;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import com.algaworks.algafood.api.model.CozinhaModel;
@@ -12,19 +14,12 @@ import com.algaworks.algafood.domain.model.Restaurante;
 @Component
 public class RestauranteModelAssembler {
 	
+	@Autowired
+	private ModelMapper modelMapper;
+	
 	public RestauranteModel toModel(Restaurante restaurante) {
-		RestauranteModel restauranteModel = new RestauranteModel();
-		
-		CozinhaModel cozinhaModel = new CozinhaModel();
-		cozinhaModel.setId(restaurante.getCozinha().getId());
-		cozinhaModel.setNome(restaurante.getCozinha().getNome());
-		
-		restauranteModel.setId(restaurante.getId());
-		restauranteModel.setNome(restaurante.getNome());
-		restauranteModel.setTaxaFrete(restaurante.getTaxaFrete());		
-		restauranteModel.setCozinha(cozinhaModel);
-		
-		return restauranteModel;
+	
+		return modelMapper.map(restaurante, RestauranteModel.class);
 	}
 	
 	public List<RestauranteModel> toCollectionModel(List<Restaurante> restaurantes){
