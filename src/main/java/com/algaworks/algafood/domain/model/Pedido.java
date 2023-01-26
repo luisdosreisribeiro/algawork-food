c8167fd2fe9bf70c9cadf1582d4dc03955d8860c
Luis Ribeiro <luis.ribeiro@unicred.com.br>
2023-01-26 20:36:25 -0300

    12.24. Refatorando o código de regras para transição de status de pedidos.

--- src/main/java/com/algaworks/algafood/domain/model/Pedido.java
+++ src/main/java/com/algaworks/algafood/domain/model/Pedido.java
@@ -7,6 +7,7 @@ import java.util.List;
 
 import javax.persistence.*;
 
+import com.algaworks.algafood.domain.exception.NegocioException;
 import org.hibernate.annotations.CreationTimestamp;
 
 import lombok.Data;
@@ -69,4 +70,27 @@ public class Pedido {
 		this.valorTotal = this.subtotal.add(this.taxaFrete);
 	}
 
+	public void confirmar(){
+		setStatus(StatusPedido.CONFIRMADO);
+		setDataConfirmacao(OffsetDateTime.now());
+	}
+
+	public void entregar(){
+		setStatus(StatusPedido.ENTREGUE);
+		setDataEntrega(OffsetDateTime.now());
+	}
+
+	public void cancelar(){
+		setStatus(StatusPedido.CANCELADO);
+		setDataCancelamento(OffsetDateTime.now());
+	}
+
+	private void setStatus(StatusPedido novoStatus){
+		if(getStatus().naoPodeAlterarPara(novoStatus)){
+			throw  new NegocioException(
+					String.format("Status do pedido %d não pode ser alterado de %s para %s", getId(), getStatus()
+									.getDescricao(),novoStatus.getDescricao()));
+		}
+		this.status= novoStatus;
+	}
 }
--- src/main/java/com/algaworks/algafood/domain/service/FluxoPedidoService.java
+++ src/main/java/com/algaworks/algafood/domain/service/FluxoPedidoService.java
@@ -22,35 +22,16 @@ public class FluxoPedidoService {
     @Transactional
     public void confirmar(Long pedidoId){
         Pedido pedido = emissaoPedidoService.buscarOuFalhar(pedidoId);
-
-        if(!pedido.getStatus().equals(StatusPedido.CRIADO)){
-            throw new NegocioException(
-                    String.format("Status do pedido %d não pode ser alterado de %s para %s", pedido.getId(),
-                    pedido.getStatus().getDescricao(),StatusPedido.CONFIRMADO.getDescricao()));
-        }
-        pedido.setStatus(StatusPedido.CONFIRMADO);
-        pedido.setDataConfirmacao(OffsetDateTime.now());
+        pedido.confirmar();
     }
-
+    @Transactional
     public void cancelar(Long pedidoId){
         Pedido pedido =  emissaoPedidoService.buscarOuFalhar(pedidoId);
-        if(!pedido.getStatus().equals(StatusPedido.CRIADO)){
-           throw new NegocioException(
-                   String.format("Status do pedido %d não pode ser alterado de %s para %s", pedido.getId(),
-                           pedido.getStatus().getDescricao(),StatusPedido.CANCELADO.getDescricao()));
-        }
-        pedido.setStatus(StatusPedido.CANCELADO);
-        pedido.setDataConfirmacao(OffsetDateTime.now());
+        pedido.cancelar();
     }
-
+    @Transactional
     public void entregar (Long pedidoId){
         Pedido pedido =  emissaoPedidoService.buscarOuFalhar(pedidoId);
-        if(!pedido.getStatus().equals(StatusPedido.CONFIRMADO)){
-            throw new NegocioException(
-                    String.format("Status do %d pedido não pode ser alterado de %s para %s ", pedido.getId(),
-                            pedido.getStatus().getDescricao(), StatusPedido.ENTREGUE.getDescricao()));
-        }
-        pedido.setStatus(StatusPedido.ENTREGUE);
-        pedido.setDataConfirmacao(OffsetDateTime.now());
+        pedido.entregar();
     }
 }
