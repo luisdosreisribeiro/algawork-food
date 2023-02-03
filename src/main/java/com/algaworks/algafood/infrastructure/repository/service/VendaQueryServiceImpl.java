ee507d2a8f0c1dfb9e51780b9b950877c0c1f07d
Luis Ribeiro <luis.ribeiro@unicred.com.br>
2023-02-03 20:14:06 -0300

    13.15. Desafio: adicionando os filtros na consulta de vendas diárias.

--- src/main/java/com/algaworks/algafood/infrastructure/repository/service/VendaQueryServiceImpl.java
+++ src/main/java/com/algaworks/algafood/infrastructure/repository/service/VendaQueryServiceImpl.java
@@ -2,14 +2,18 @@ package com.algaworks.algafood.infrastructure.repository.service;
 
 import com.algaworks.algafood.domain.filter.VendaDiariaFilter;
 import com.algaworks.algafood.domain.model.Pedido;
+import com.algaworks.algafood.domain.model.StatusPedido;
 import com.algaworks.algafood.domain.model.dto.VendaDiaria;
 import com.algaworks.algafood.domain.service.VendaQueryService;
 import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.criteria.Predicate;
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
+
 
 @Repository
 public class VendaQueryServiceImpl implements VendaQueryService {
@@ -33,8 +37,22 @@ public class VendaQueryServiceImpl implements VendaQueryService {
                 builder.count(root.get("id")),
                 builder.sum(root.get("valorTotal")));
 
+        var predicates = new ArrayList<Predicate>();
+
+        if(filtro.getRestauranteId()!= null){
+            predicates.add(builder.equal(root.get("restaurante"),filtro.getRestauranteId()));
+        }
+        if(filtro.getDataCriacaoInicio() != null){
+            predicates.add(builder.greaterThanOrEqualTo(root.get("dataCriacao"),filtro.getDataCriacaoInicio()));
+        }
+        if(filtro.getDataCriacaoFim() != null){
+            predicates.add(builder.lessThanOrEqualTo(root.get("dataCriacao"),filtro.getDataCriacaoFim()));
+        }
+        predicates.add(root.get("status").in(StatusPedido.CONFIRMADO, StatusPedido.ENTREGUE));
+
         query.select(selection);
         query.groupBy(functionDateDateCriacao);
+        query.where(predicates.toArray(new Predicate[0]));
 
         return  manager.createQuery(query).getResultList();
     }
