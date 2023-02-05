201f446458537242fe162320cff8c01fe08e1802
Luis Ribeiro <luis.ribeiro@unicred.com.br>
2023-02-04 21:35:52 -0300

    14.3. Validando o tamanho máximo do arquivo.

--- src/main/java/com/algaworks/algafood/api/controller/RestauranteProdutoFotoController.java
+++ src/main/java/com/algaworks/algafood/api/controller/RestauranteProdutoFotoController.java
@@ -1,10 +1,14 @@
 package com.algaworks.algafood.api.controller;
 
 import com.algaworks.algafood.api.model.input.FotoProdutoInput;
-import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.web.multipart.MultipartFile;
 
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
 import java.nio.file.Path;
 import java.util.UUID;
 
@@ -13,7 +17,7 @@ import java.util.UUID;
 public class RestauranteProdutoFotoController {
     @PutMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     public void atualizarFoto(@PathVariable Long restauranteId, @PathVariable Long produtoId,
-                              FotoProdutoInput fotoProdutoInput){
+                             @Valid FotoProdutoInput fotoProdutoInput){
 
 
         var nomeArquivo = UUID.randomUUID().toString()
