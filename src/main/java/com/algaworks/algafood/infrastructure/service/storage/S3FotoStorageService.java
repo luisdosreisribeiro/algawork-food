e4108c2c038f3b90082a7c5a4569f3d908275402
Luis Ribeiro <luis.ribeiro@unicred.com.br>
2023-02-24 07:42:31 -0300

    14.22. Definindo bean do client da Amazon S3 e configurando credenciais.

--- src/main/java/com/algaworks/algafood/infrastructure/service/storage/S3FotoStorageService.java
+++ src/main/java/com/algaworks/algafood/infrastructure/service/storage/S3FotoStorageService.java
@@ -1,12 +1,15 @@
 package com.algaworks.algafood.infrastructure.service.storage;
 
 import com.algaworks.algafood.domain.service.FotoStorageService;
+import com.amazonaws.services.s3.AmazonS3;
 import org.springframework.stereotype.Service;
 
 import java.io.InputStream;
 
 @Service
 public class S3FotoStorageService implements FotoStorageService {
+
+    private AmazonS3 amazonS3;
     @Override
     public InputStream recuperar(String nomeArquivo) {
         return null;
