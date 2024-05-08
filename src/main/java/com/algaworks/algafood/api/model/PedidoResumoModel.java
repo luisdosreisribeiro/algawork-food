177a3cc1a2b6f18667cba97fd185409dd6e86cc9
Luis Ribeiro <luis.ribeiro@e-unicred.com.br>
2024-05-08 19:59:24 -0300

    13.2. Limitando os campos retornados pela API com @JsonFilter do Jackson.

--- src/main/java/com/algaworks/algafood/api/model/PedidoResumoModel.java
+++ src/main/java/com/algaworks/algafood/api/model/PedidoResumoModel.java
@@ -2,6 +2,7 @@ package com.algaworks.algafood.api.model;
 
 import com.algaworks.algafood.api.model.input.ItemPedidoModel;
 import com.algaworks.algafood.domain.model.StatusPedido;
+import com.fasterxml.jackson.annotation.JsonFilter;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -10,6 +11,7 @@ import java.time.OffsetDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
+@JsonFilter("pedidoFilter")
 @Getter
 @Setter
 public class PedidoResumoModel {
