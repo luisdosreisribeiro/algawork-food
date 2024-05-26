455f817fc2dff0cc669de4672409bddced05cf50
Luis Ribeiro <luis.ribeiro@e-unicred.com.br>
2024-05-26 20:34:39 -0300

    Relatório - 13.20. Preenchendo um relatório JasperReports com JavaBeans e gerando bytes do PDF.

--- src/main/java/com/algaworks/algafood/api/controller/EstatisticasController.java
+++ src/main/java/com/algaworks/algafood/api/controller/EstatisticasController.java
@@ -3,7 +3,12 @@ package com.algaworks.algafood.api.controller;
 import com.algaworks.algafood.domain.filter.VendaDiariaFilter;
 import com.algaworks.algafood.domain.model.dto.VendaDiaria;
 import com.algaworks.algafood.domain.service.VendaQueryService;
+import com.algaworks.algafood.domain.service.VendaReportService;
+import net.sf.jasperreports.engine.JRException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,10 +20,26 @@ import java.util.List;
 public class EstatisticasController {
 
     @Autowired
+    private VendaReportService vendaReportService;
+
+    @Autowired
     private VendaQueryService vendaQueryService;
 
-    @GetMapping("/vendas-diarias")
+    @GetMapping(path = "/vendas-diarias", produces = MediaType.APPLICATION_JSON_VALUE)
     public List<VendaDiaria> consultarVendasDiarias(VendaDiariaFilter filtro) {
         return vendaQueryService.consultaVendasDiarias(filtro);
+    }
+
+    @GetMapping(path = "/vendas-diarias", produces = MediaType.APPLICATION_PDF_VALUE)
+    public ResponseEntity<byte[]> consultarVendasDiariasPdf(VendaDiariaFilter filtro) throws JRException {
+        byte[] bytesPdf = vendaReportService.emitirVendasDiarias(filtro);
+
+        var headers = new HttpHeaders();
+        headers.add(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=venda-diaria.pdf");
+
+        return ResponseEntity.ok()
+                .contentType(MediaType.APPLICATION_PDF)
+                .headers(headers)
+                .body(bytesPdf);
     }
 }
